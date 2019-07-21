8ad6f4c1a794a1e3c06043679a9af18298c99b9c
Nasim Kabiliravi <kabiliravi@gmail.com>
2019-07-21 17:44:10 -0400

    Added sample for praticing boolean type

---
+++ TryBoolean.java
@@ -0,0 +1,11 @@
+public class TryBoolean {
+  public static void main(String[] args) {
+     boolean tryMe1 = false;
+     System.out.print("tryMe1 value: ");
+     System.out.println(tryMe1);
+
+     boolean tryMe2 = true;
+     System.out.print("tryMe2 value: ");
+     System.out.println(tryMe2);
+  }
+}
