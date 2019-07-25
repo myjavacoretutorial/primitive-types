7697028098d1821e2adbdf0bce6506a3a3072fc3
Nasim Kabiliravi <kabiliravi@gmail.com>
2019-07-24 20:35:21 -0400

    Added Byte and Char samples

---
+++ TryByte.java
@@ -0,0 +1,11 @@
+public class TryByte {
+  public static void main(String[] args) {
+     byte minByte = -128;
+     System.out.print("minByte value: ");
+     System.out.println(minByte);
+
+     byte maxByte = 127;
+     System.out.print("maxByte value: ");
+     System.out.println(maxByte);
+  }
+}
---
+++ TryChar.java
@@ -0,0 +1,15 @@
+public class TryChar {
+  public static void main(String[] args) {
+     char tryChar1 = 'A';
+     System.out.print("tryChar1 char value: ");
+     System.out.println(tryChar1);
+     System.out.print("tryChar1 ascii code: ");
+     System.out.println((short) tryChar1);
+
+     char tryChar2 = 'ß';
+     System.out.print("tryChar2 value: ");
+     System.out.println(tryChar2);
+     System.out.print("tryChar2 ascii code: ");
+     System.out.println((short) tryChar2);
+  }
+}
