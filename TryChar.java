ed4b1151be1b30eb2229c4db7ab813030c40b774
Nasim Kabiliravi <nasim_kabiliravi1@homedepot.com>
2019-08-20 13:24:37 -0400

    Completed TryChar sample

--- TryChar.java
+++ TryChar.java
@@ -4,12 +4,24 @@ public class TryChar {
      System.out.print("tryChar1 char value: ");
      System.out.println(tryChar1);
      System.out.print("tryChar1 ascii code: ");
-     System.out.println((short) tryChar1);
+     System.out.println((int) tryChar1);
 
      char tryChar2 = 'ß';
      System.out.print("tryChar2 value: ");
      System.out.println(tryChar2);
      System.out.print("tryChar2 ascii code: ");
-     System.out.println((short) tryChar2);
+     System.out.println((int) tryChar2);
+
+     char tryChar3 = 124;
+     System.out.print("tryChar3 value: ");
+     System.out.println(tryChar3);
+     System.out.print("tryChar3 ascii code: ");
+     System.out.println((int) tryChar3);
+
+     char tryChar4 = '\u2142';
+     System.out.print("tryChar4 value: ");
+     System.out.println(tryChar4);
+     System.out.print("tryChar4 Unicode code: ");
+     System.out.println((int) tryChar4);
   }
 }
